4dc7f3024f89a17bc78478ea0d4463badb0fa48c
nasty <anastaishastil2002@gmail.com>
2022-09-04 18:45:16 +0300

    Updated

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -19,7 +19,7 @@ public class SwitchDaysInMonthPrinter {
                 System.out.println(30);
                 break;
             case 2:
-                System.out.println(29);
+                System.out.println(28);
                 break;
             default:
                 System.out.println("wrong number!");
